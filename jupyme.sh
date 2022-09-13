c6d1eb7e6c40d6d222d2b19d3b8d41ad61db9c65
Mike Levin <miklevin@gmail.com>
2022-09-13 18:05:00 -0400

    Refining script

--- jupyme.sh
+++ jupyme.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 CONTAINERS="$(lxc ls -c "n" --format csv | grep jupyter)"
-
 for container in $CONTAINERS; do
   echo "Stopping $container..."
   lxc stop $container
 done
 
 JUPYTER_EXISTS="$(lxc ls -c "n" --format csv | grep jupyter)"
-
 if [ "$JUPYTER_EXISTS" == "jupyter" ]; then
   lxc delete jupyter --force
   echo "Old Jupyter container deleted."
@@ -24,18 +22,21 @@ do
   sleep 2
 done
 
-lxc exec jupyter -- sudo --login --user ubuntu bash -ilc "mkdir /home/ubuntu/repos"
-lxc exec jupyter -- sudo --login --user ubuntu bash -ilc "add-apt-repository ppa:deadsnakes/ppa -y"
-lxc exec jupyter -- sudo --login --user ubuntu bash -ilc "add-apt-repository ppa:deadsnakes/ppa -y"
+lxc exec jupyter -- add-apt-repository ppa:deadsnakes/ppa -y
 lxc exec jupyter -- apt update
 lxc exec jupyter -- apt upgrade -y
 lxc exec jupyter -- apt autoremove -y
-lxc exec jupyter -- apt install figlet
-figlet foo
-# lxc exec jupyter -- sudo apt install python3.10 -y
-# lxc exec jupyter -- sudo apt install python3.10-venv -y
-# lxc exec jupyter -- sudo --login --user ubuntu bash -ilc "/usr/bin/python3 -m venv /home/ubuntu/py310"
-# lxc exec jupyter -- sudo --login --user ubuntu bash -ilc "https://raw.githubusercontent.com/miklevin/jupyme/main/.bash_profile"
-# lxc exec jupyter -- sudo --login --user ubuntu bash -ilc "https://raw.githubusercontent.com/miklevin/jupyme/main/.bash_prompt"
+lxc exec jupyter -- apt install python3.10 -y
+lxc exec jupyter -- apt install python3.10-venv -y
+
+lxc exec jupyter -- su --login ubuntu bash -c "/usr/bin/python3.10 -m venv /home/ubuntu/py310"
+lxc exec jupyter -- su --login ubuntu bash -c "echo 'source ~/py310/bin/activate' > /home/ubuntu/.bash_profile"
+
+# lxc exec jupyter -- su --login ubuntu bash -c "https://raw.githubusercontent.com/miklevin/jupyme/main/.bash_prompt"
+# lxc exec jupyter -- su --login ubuntu bash -c "https://raw.githubusercontent.com/miklevin/jupyme/main/.bash_profile"
 # lxc exec jupyter -- sudo --login --user ubuntu bash -ilc "https://raw.githubusercontent.com/miklevin/jupyme/main/.screenrc"
+# WIN_HOME="$(printenv | grep -o '/mnt/c/Users/[a-zA-Z]*/')"
+# lxc config device add jupyter repos disk source=${WIN_HOME}repos path=/home/ubuntu/repos
+# lxc config device add jupyter ssh disk source=${WIN_HOME}.ssh/ path=/home/ubuntu/.ssh/
+# 
 # lxc exec jupyter -- sudo --login --user ubuntu bash -ilc "/home/ubuntu/py310/bin/python3.10 -m pip install jupyterlab"
