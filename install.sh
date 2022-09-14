b28043a2bc558755846c100a3c314d0c79c8e3f7
Mike Levin <miklevin@gmail.com>
2022-09-13 20:13:36 -0400

    Tiny tweak

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 echo "You must have a repos and .ssh folder in your home folder C:\\Users\\[username]"
 echo "This will replace your current Jupyter Container, config and pip installs."
-echo ""
-read -p "Are you sure you want to continue?" -n 1 -r
-echo    # (optional) move to a new line
+echo
+read -p "Are you sure you want to continue? " -n 1 -r
+echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
