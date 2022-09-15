669cc9ae5520c466a35939f1b55a1c5a8f8f1124
Mike Levin <miklevin@gmail.com>
2022-09-14 23:43:55 -0400

    Rough success

--- lxdme_windows.bat
+++ lxdme_windows.bat
@@ -17,13 +17,18 @@ set /p warning=Press enter to continue...
 echo.
 
 mkdir %USERPROFILE%\repos
+rmdir /S /Q %USERPROFILE%\repos\temp
 mkdir %USERPROFILE%\repos\temp
 wsl --set-default-version 2
 wsl --set-default Ubuntu-18.04
 wsl -u root -e echo -e "[automount]\n"options = \"metadata\" > wsl.conf
 wsl -u root -e mv wsl.conf /etc/
+wsl -u root -e ln -s /mnt/c/Users/%USERNAME%/.ssh/ /home/ubuntu/.ssh
+wsl -u root -e ln -s /mnt/c/Users/%USERNAME%/repos/ /home/ubuntu/repos
+
 wsl --shutdown
-wsl -e bash -lic "ln -s /mnt/c/Users/%USERNAME%/repos /home/ubuntu/ >/dev/null 2>&1"
+wsl -e bash -lic "cp /mnt/c/Users/%USERNAME%/.gitconfig /home/ubuntu/"
+wsl -e bash -lic "cp /mnt/c/Users/%USERNAME%/.vimrc /home/ubuntu/"
 
 wsl -u root -e curl -L -o /home/ubuntu/repos/temp/install.sh "https://raw.githubusercontent.com/nullpo-head/wsl-distrod/main/install.sh"
 wsl -u root -e chmod 777 /home/ubuntu/repos/temp/install.sh
@@ -31,7 +36,7 @@ wsl -u root -e chown ubuntu:ubuntu /home/ubuntu/repos/temp/install.sh
 wsl -u root -e /home/ubuntu/repos/temp/install.sh install
 wsl -u root -e /opt/distrod/bin/distrod enable
 
-wsl -u root -e  lxd init --auto
+wsl -- sudo lxd init --auto
 wsl -u root -e curl -L -o /home/ubuntu/repos/temp/install.sh "https://raw.githubusercontent.com/miklevin/jupyme/main/install.sh"
 wsl -u root -e chmod 777 /home/ubuntu/repos/temp/install.sh
 wsl -u root -e chown ubuntu:ubuntu /home/ubuntu/repos/temp/install.sh
