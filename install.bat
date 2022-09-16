da341d828797c2d3ad92d058c445680e85821be2
Mike Levin <miklevin@gmail.com>
2022-09-16 14:19:51 -0400

    Added todo list

--- install.bat
+++ install.bat
@@ -1,5 +1,6 @@
-cls
 @echo off
+cls
+
 :::intro:::  Begin your life-time journey of Linux, Python, vim & git.         /)
 :::intro:::      _                   _          __   __                 /)\_ _//
 :::intro:::     | |_   _ _ __  _   _| |_ ___ _ _\ \ / /__  _   _    ___(/_ 0 0
@@ -7,17 +8,25 @@ cls
 :::intro::: | |_| | |_| | |_) | |_| | ||  __/ |   | | (_) | |_| |   \  )   \"\
 :::intro:::  \___/ \__,_| .__/ \__, |\__\___|_|   |_|\___/ \__,_|    |__>-\_>_>
 :::intro:::             |_|    |___/
+:::intro::: RUN JUPYTER ON LXD LINUX CONTAINER UNDER WINDOWS SUBSYSTEM FOR LINUX
 
 :: Welcome to the most amazing thing you're going to see all day, and perhaps one
-:: that will change your life forever. Enough hype, let's get on with the thing
-:: Microsoft doesn't want you to see, and certainly not be able to do. Simply
-:: right-click in this window (if you're viewing it from the Github raw view at
+:: that will change your life forever. Enough hype. Let's get on with the process
+:: that Microsoft doesn't want you to see -- and certainly not be able to perform.
+
+:: HOW TO INSTALL (SAVE THIS AND RUN)
+
+:: Right-click in this window (if you're viewing it from the Github raw view at
 :: https://raw.githubusercontent.com/miklevin/wsl2lxd/main/install.bat ), select
 :: all, create a file named install.bat on your local Windows 10 or 11 computer
-:: and double-click it to run. It will take over your Ubuntu 18.04 "slot" in the
-:: Windows Subsysystem for Linux, then install the LXD Linux Conainer software
-:: with an Ubuntu 20.04 hosting JupyterLab Server reachable on your Windows
-:: machine at http://localhost:8888 Step #3: Heal The World!
+:: and double-click it to run. 
+
+:: WHAT DOES IT DO? (INSTALLS A PORTABLE LINUX)
+
+:: It will take over your Ubuntu 18.04 "slot" in the Windows Subsysystem for Linux,
+:: (WSL) then install the LXD Linux Conainer software with an Ubuntu 20.04 hosting
+:: JupyterLab Server reachable on your Windows machine at http://localhost:8888 
+:: This the best way to run Juputer and eventually free yourself from Windows.
 
 :: Display the above bunny-banner when the script is run as Windows .BAT file.
 for /f "delims=: tokens=1*" %%A in ('findstr /b ":::intro:::" "%~f0"') do (echo.%%B)
@@ -115,3 +124,15 @@ wsl --distribution Ubuntu-18.04 -u ubuntu -e bash /home/ubuntu/repos/temp/instal
 
 :: See the rest at https://raw.githubusercontent.com/miklevin/jupyme/main/install.sh
 :: but you don't have to do anything because it is downloaded and run from above.
+
+::  _____     ____
+:: |_   _|__ |  _ \  ___
+::   | |/ _ \| | | |/ _ \
+::   | | (_) | |_| | (_) |
+::   |_|\___/|____/ \___/
+
+:: - Provide drag-copy shortcut in repo to run server after reboot.
+:: - Switch Jupyter startup method from .bash_profile to Linux service.
+:: - Prevent Jupyter configuration from being wiped-out by reinstalls.
+:: - Copy requirements.txt LXD-side and one-time pip install -r requirements.txt
+:: - Copy apt_installs.txt LXD-side and one-time source avtivate them.
