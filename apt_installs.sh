1643214871e589524ad0be32d61d150ecbf9c892
Mike Levin <miklevin@gmail.com>
2022-09-21 18:21:47 -0400

    Cleaning up post installer files

--- apt_installs.sh
+++ apt_installs.sh
@@ -1,3 +1,11 @@
+# Figlet is available for banner messages
+figlet -t Installing Google Chrome
+
+# Install Google Chrome Dependencies
 sudo apt-get install libxss1 libappindicator1 libindicator7 -y
+
+# Get the Google Chrome debian installer
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# Install Google Chrome using hte local installer
 sudo apt install ./google-chrome*.deb -y
