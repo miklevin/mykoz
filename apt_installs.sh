b5e7b5456d96b43b9770d77af1bb3f91a90285fe
Mike Levin <miklevin@gmail.com>
2022-09-21 14:50:06 -0400

    Chrome test

--- apt_installs.sh
+++ apt_installs.sh
@@ -1,1 +1,3 @@
-sudo apt install x11-apps -y
+sudo apt-get install libxss1 libappindicator1 libindicator7 -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome*.deb -y
