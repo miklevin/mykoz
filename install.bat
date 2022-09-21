db8e8adf333bdc1882c487dd12d9bfdd52fa5b76
Mike Levin <miklevin@gmail.com>
2022-09-21 11:47:22 -0400

    Fixed silly repo rename oversight

--- install.bat
+++ install.bat
@@ -128,8 +128,8 @@ wsl --distribution Ubuntu-18.04 -u ubuntu -e lxd init --auto
 wsl --distribution Ubuntu-18.04 -e bash -lic "figlet -t 'Installing LXD Container'"
 
 :: Grab and run second-half of install that runs under WSL and set up Linux graphics.
-wsl --distribution Ubuntu-18.04 -u ubuntu -e curl -L -o /home/ubuntu/install.sh "https://raw.githubusercontent.com/miklevin/jupyme/main/install.sh"
-wsl --distribution Ubuntu-18.04 -u ubuntu -e curl -L -o /home/ubuntu/.bash_profile "https://raw.githubusercontent.com/miklevin/jupyme/main/.bash_profile-wsl"
+wsl --distribution Ubuntu-18.04 -u ubuntu -e curl -L -o /home/ubuntu/install.sh "https://raw.githubusercontent.com/miklevin/lxdwin/main/install.sh"
+wsl --distribution Ubuntu-18.04 -u ubuntu -e curl -L -o /home/ubuntu/.bash_profile "https://raw.githubusercontent.com/miklevin/lxdwin/main/.bash_profile-wsl"
 wsl --distribution Ubuntu-18.04 -e bash -lic "figlet -t 'Updating LXD Linux'"
 wsl --distribution Ubuntu-18.04 -u ubuntu -e bash /home/ubuntu/install.sh
 
