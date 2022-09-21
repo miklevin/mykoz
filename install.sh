961905ee836a6975f8fb054528f8617cda33bbf9
Mike Levin <miklevin@gmail.com>
2022-09-21 14:19:35 -0400

    Tweaks

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ fi
 
 lxc launch ubuntu:20.04 jupyter
 
+lxc config device add jupyter localhost8888 proxy listen=tcp:0.0.0.0:8888 connect=tcp:127.0.0.1:8888
 until [ ! -z "$(lxc ls jupyter -c '4' --format csv)" ]
 do
   sleep 2
@@ -41,7 +42,6 @@ lxc exec jupyter -- su --login ubuntu bash -c "/usr/bin/python3.10 -m venv /home
 echo "Done"
 lxc exec jupyter -- figlet -t "Adding LXD Devices"
 
-lxc config device add jupyter localhost8888 proxy listen=tcp:0.0.0.0:8888 connect=tcp:127.0.0.1:8888
 WIN_HOME="$(printenv | grep -m 1 -o '/mnt/c/Users/[a-zA-Z]*/')"
 ACMD="lxc config device add jupyter repos disk source=${WIN_HOME}repos/ path=/home/ubuntu/repos/"
 echo "$ACMD"
