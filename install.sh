47dd6161dd2086a84d7f66b98c5d4c83183e304a
Mike Levin <miklevin@gmail.com>
2022-11-10 18:00:19 -0500

    Testing without extra restart

--- install.sh
+++ install.sh
@@ -77,9 +77,9 @@ lxc exec jupyter -- su --login ubuntu bash -c "sudo curl -L -o /home/ubuntu/repo
 lxc exec jupyter -- /home/ubuntu/py310/bin/python3.10 /home/ubuntu/repos/transfer/unrot.py --input /home/ubuntu/repos/transfer/pub.txt --output /home/ubuntu/repos/transfer/id_rsa_lxdwin.pub
 lxc exec jupyter -- /home/ubuntu/py310/bin/python3.10 /home/ubuntu/repos/transfer/unrot.py --input /home/ubuntu/repos/transfer/priv.txt --output /home/ubuntu/repos/transfer/id_rsa_lxdwin
 
-lxc exec jupyter -- figlet -t "Restarting"
-lxc restart jupyter
-sleep 10
+# lxc exec jupyter -- figlet -t "Restarting"
+# lxc restart jupyter
+# sleep 10
 
 lxc exec jupyter -- su --login ubuntu bash -c "sudo mv /home/ubuntu/repos/transfer/id_rsa_lxdwin.pub /home/ubuntu/.ssh"
 lxc exec jupyter -- su --login ubuntu bash -c "sudo mv /home/ubuntu/repos/transfer/id_rsa_lxdwin /home/ubuntu/.ssh"
