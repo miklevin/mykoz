3b1c78b2367766c6a26dc62441632b7a2cd5b1f3
Mike Levin <miklevin@gmail.com>
2022-11-13 09:04:58 -0500

    Figlet headline fix

--- git_installs.sh
+++ git_installs.sh
@@ -1,5 +1,4 @@
-figlet -t "Upgrading LXD Container"
-echo "Cloning git repos..."
+figlet -t "Cloining git repos"
 cd /home/ubuntu/repos
 [ ! -d "/home/ubuntu/repos/mlseo" ] && git clone git@github.com:miklevin/mlseo /home/ubuntu/repos/mlseo
 [ ! -d "/home/ubuntu/repos/ohawf" ] && git clone git@github.com:miklevin/mlseo /home/ubuntu/repos/ohawf
