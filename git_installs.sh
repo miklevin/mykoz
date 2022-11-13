80e6522c0fc29861c459a7d162b1259c114c791a
Mike Levin <miklevin@gmail.com>
2022-11-13 10:03:37 -0500

    It's working

--- git_installs.sh
+++ git_installs.sh
@@ -1,6 +1,8 @@
-figlet -t "Cloining git repos"
+figlet -t "Cloning git repos."
 cd /home/ubuntu/repos
+figlet -t "Cloning mlseo"
 [ ! -d "/home/ubuntu/repos/mlseo" ] && git clone git@github.com:miklevin/mlseo /home/ubuntu/repos/mlseo
+figlet -t "Cloning ohawf"
 [ ! -d "/home/ubuntu/repos/ohawf" ] && git clone git@github.com:miklevin/mlseo /home/ubuntu/repos/ohawf
-echo "Done git cloning."
+figlet -t "Done cloning."
 
