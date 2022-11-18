53fdbb4b98d525b91a6121cbb420e3e764a5a022
Mike Levin <miklevin@gmail.com>
2022-11-18 18:47:54 -0500

    Improved messaging

--- install.bat
+++ install.bat
@@ -2,19 +2,13 @@
 cls
 
 
-:::intro:::   ____ _                      __  __      _                     _. 
-:::intro:::  / ___| |__   __ _ ___  ___  |  \/  | ___| |               /)  /  \     
-:::intro::: | |   | '_ \ / _` / __|/ _ \ | |\/| |/ _ \ |        /)\_ _//  /    |    
-:::intro::: | |___| | | | (_| \__ \  __/ | |  | |  __/_|    ___(/_ 0 0    |LXD |    
-:::intro:::  \____|_| |_|\__,_|___/\___| |_|  |_|\___(_)  *(     =(_T_)=  | WIN|    
-:::intro:::                                                 \  )   \"\    \    /   
-:::intro:::            Linux, Python, vim & git!             |__>-\_>_>    \__/
-:::intro:::
-:::intro::: Do you ever get the feeling that the things you're spending your time
-:::intro::: on are not building the CORRECT long-term skills for life? Join me in
-:::intro::: chasing the timeless tools of tech: Linux, Python, vim & git and make
-:::intro::: yourself part of the most literate generation to ever walk the Earth.
-:::intro::: 
+:::intro:::   ____ _                      __  __      _                        _. 
+:::intro:::  / ___| |__   __ _ ___  ___  |  \/  | ___| |                /)    /  \     
+:::intro::: | |   | '_ \ / _` / __|/ _ \ | |\/| |/ _ \ |         /)\_ _//    /    |    
+:::intro::: | |___| | | | (_| \__ \  __/ | |  | |  __/_|     ___(/_ 0 0      |LXD |    
+:::intro:::  \____|_| |_|\__,_|___/\___| |_|  |_|\___(_)   *(     =(_T_)=    | WIN|    
+:::intro:::                                                  \  )   \"\      \    /   
+:::intro:::            Linux, Python, vim & git!              |__>-\_>_>      \__/
 
 :: LIFE AFTER WINDOWS STARTS HERE
 :: Welcome to the most amazing thing you're going to see all day, and perhaps one
@@ -36,32 +30,48 @@ cls
 
 :: WHAT DOES IT DO? (INSTALLS A PORTABLE LINUX)
 :: It will take over your Ubuntu 18.04 "slot" in the Windows Subsysystem for Linux,
-:: (WSL) then install the LXD Linux Conainer software with an Ubuntu 20.04 hosting
+:: (WSL) then install the LXD Linux Container software with an Ubuntu 20.04 hosting
 :: JupyterLab Server reachable on your Windows machine at http://localhost:8888 
-:: This the best way to run Juputer and eventually free yourself from Windows.
+:: This the best way to run Jupyter and eventually free yourself from Windows.
 
 :: Display the above bunny-banner when the script is run as Windows .BAT file.
 for /f "delims=: tokens=1*" %%A in ('findstr /b ":::intro:::" "%~f0"') do (echo.%%B)
+echo.
+echo  Do you ever get the feeling that you spend your time learning the wrong
+echo  tools for developing life-long skills? If so, then join me in chasing
+echo  the increasingly timeless tools of tech: Linux, Python, vim and git.
+echo.
+echo  You stand at the precipice of a great adventure. The next screen will
+echo  tell you what changes this script will do to your system and give you
+echo  another chance to exit. Are you brave enough to continue?
+
+echo.
 echo                      Press [Enter] to jump down the Linux rabbit hole...
 echo                                (or press Ctrl+C escape.)
 set /p warning= %
 
 cls
-echo Excellent choice! When you hit Enter, you will be falling
-echo down the rabbit-hole and see a whole bunch of stuff scroll
-echo past you. You'll need a good Interent connection for it to
-echo work, but don't worry, the fall will end, and you will be
-echo able to reach JypyterLab from your web browser, and log in
-echo to the Linux container through a text-based user interface.
 echo.
-echo WHAT LXDWIN SCRIPT DOES:
-echo - This will uninstall any previous Ubuntu-18.04 under WSL.
-echo - Any other Linuxes running under WSL will be restarted.
-echo - A new Ubuntu 18.04 will be installed to host LXD Containers.
-echo - JupyterLab will be reachable at http://localhost:8888
-set /p warning=Press [Enter] to jump down the Linux rabbit hole or press Ctrl+C escape. %
+echo  Excellent choice! Contrary to popular belief, Linux is not a desktop
+echo  operating system. This script will install Linux under Windows WSL
+echo  and use it to "invisibly" host JupyterLab, reachable with a browser.
+echo. 
+echo  The Linux you will be using is "containerized" and can exist completely
+echo  separate from Microsoft, so this sets you on a good path. Everything you
+echo  learn is portable across hardware, cloud platforms and the ages.
+echo. 
+echo  Two copies of Linux will be put on your system: Ubuntu-18.04 to run
+echo  directly on Windows to host LXD containers, and Ubuntu 20.04 as the 
+echo  actual LXD container. You will need a good Internet connection.
+echo. 
+echo  - This will uninstall any previous Ubuntu-18.04 under WSL.
+echo  - Any other Linuxes running under WSL are left untouched.
+echo  - A new Ubuntu 18.04 will be installed to host LXD Containers.
+echo  - JupyterLab will be reachable at http://localhost:8888
+echo.
+echo  Press [Enter] to install Linux, or press Ctrl+C to exit.
+set /p warning= %
 
-:: If you're runnnig Ubuntu 18.04 under WSL, this is going to delete it!
 wsl --unregister Ubuntu-18.04
 wsl --set-default-version 2
 
