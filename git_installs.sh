c9413db6fb2405af05845e77ae0f9c3dbe284b67
Mike Levin <miklevin@gmail.com>
2022-11-22 16:02:22 -0500

    Testing again

--- git_installs.sh
+++ git_installs.sh
@@ -1,8 +1,8 @@
 figlet -t "Cloning git repos."
 cd /home/ubuntu/repos
-figlet -t "Cloning mlseo"
+echo "Cloning mlseo..."
 [ ! -d "/home/ubuntu/repos/mlseo" ] && git clone git@github.com:miklevin/mlseo /home/ubuntu/repos/mlseo
-figlet -t "Cloning ohawf"
+echo "Cloning ohawf..."
 [ ! -d "/home/ubuntu/repos/ohawf" ] && git clone git@github.com:miklevin/mlseo /home/ubuntu/repos/ohawf
 figlet -t "Done cloning."
 
