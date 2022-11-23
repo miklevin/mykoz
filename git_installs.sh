47fcfbd8cd24dda4a3c4c0f276260c0e542cf6dc
Ubuntu <ubuntu@jupyter.lxd>
2022-11-23 19:48:30 +0000

    Fixed git cloning

--- git_installs.sh
+++ git_installs.sh
@@ -2,7 +2,7 @@ figlet -t "Cloning git repos..."
 sleep 2
 cd /home/ubuntu/repos
 echo "Cloning ohawf..."
-[ ! -d "/home/ubuntu/repos/ohawf" ] && git clone git@github.com:miklevin/mlseo /home/ubuntu/repos/ohawf
+[ ! -d "/home/ubuntu/repos/ohawf" ] && git clone git@github.com:miklevin/ohawf /home/ubuntu/repos/ohawf
 pip install -e /home/ubuntu/repos/ohawf
 echo "Cloning mlseo..."
 [ ! -d "/home/ubuntu/repos/mlseo" ] && git clone git@github.com:miklevin/mlseo /home/ubuntu/repos/mlseo
