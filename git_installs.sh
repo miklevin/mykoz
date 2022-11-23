751b67dff2f30056c2293af7e792aeb7e41f52fe
Mike Levin <miklevin@gmail.com>
2022-11-23 11:36:51 -0500

    Adding pip installs post git clone

--- git_installs.sh
+++ git_installs.sh
@@ -1,8 +1,10 @@
 figlet -t "Cloning git repos."
 cd /home/ubuntu/repos
-echo "Cloning mlseo..."
-[ ! -d "/home/ubuntu/repos/mlseo" ] && git clone git@github.com:miklevin/mlseo /home/ubuntu/repos/mlseo
 echo "Cloning ohawf..."
 [ ! -d "/home/ubuntu/repos/ohawf" ] && git clone git@github.com:miklevin/mlseo /home/ubuntu/repos/ohawf
+pip install -e /home/ubuntu/repos/ohawf
+echo "Cloning mlseo..."
+[ ! -d "/home/ubuntu/repos/mlseo" ] && git clone git@github.com:miklevin/mlseo /home/ubuntu/repos/mlseo
+pip install -e /home/ubuntu/repos/mlseo
 figlet -t "Done cloning."
 
