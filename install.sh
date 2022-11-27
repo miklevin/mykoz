c1bf65f379a9b558795d24559f84448908a013a0
Mike Levin <miklevin@gmail.com>
2022-11-27 12:25:04 -0500

    Adapting to wslg on Windows 10

--- install.sh
+++ install.sh
@@ -57,6 +57,16 @@ eval "$ACMD"
 ACMD="lxc config device add jupyter config disk source=${WIN_HOME}.config/ path=/home/ubuntu/.config/"
 echo "$ACMD"
 eval "$ACMD"
+ACMD="lxc config device add jupyter wslg disk source=/mnt/wslg/ path=/mnt/wslg/"
+echo "$ACMD"
+eval "$ACMD"
+# ACMD="lxc config device add jupyter config disk source=/tmp/.X11-unix/ path=/tmp/.X11-unix/"
+# echo "$ACMD"
+# eval "$ACMD"
+
+lxc config set jupyter environment.WAYLAND_DISPLAY=wayland-0
+lxc config set jupyter environment.XDG_RUNTIME_dir=/mnt/wslg/runtime-dir
+lxc config set jupyter environment.PULSE_SERVER=/mnt/wslg/pulseserver
 
 echo .bash_profile
 lxc exec jupyter -- su --login ubuntu bash -c "sudo curl -L -o /home/ubuntu/.bash_profile https://raw.githubusercontent.com/miklevin/lxdwin/main/.bash_profile"
