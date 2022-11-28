acd3497ea3370ae16361869088753a0d02cba7f1
Mike Levin <miklevin@gmail.com>
2022-11-28 16:49:59 -0500

    Big trimming

--- apt_installs.sh
+++ apt_installs.sh
@@ -1,20 +1,14 @@
-# Figlet is available for banner messages
+# Standard bash file for doing late-stage apt installs
+
 figlet -t Installing Google Chrome
-
-# Install Google Chrome Dependencies
 sudo apt-get install libxss1 libappindicator1 libindicator7 -y
-
-# Get the Google Chrome debian installer
 cd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
-# Install Google Chrome using hte local installer
 sudo apt install ./google-chrome*.deb -y
+sudo apt install x11-apps -y
 
 # Install Microsoft Playright browsers, edited out in favor of Chrome
 # playwright install
-
 # Install teh default Pypeteer Chromium browser, edited out in favor of Chrome
 # pyppeteer-install
 
-sudo apt install x11-apps -y
--- old/install.bat
+++ old/install.bat
@@ -3,8 +3,8 @@ cls
 :::intro:::   ____ _                      __  __      _                        _. 
 :::intro:::  / ___| |__   __ _ ___  ___  |  \/  | ___| |                /)    /  \     
 :::intro::: | |   | '_ \ / _` / __|/ _ \ | |\/| |/ _ \ |         /)\_ _//    /    |    
-:::intro::: | |___| | | | (_| \__ \  __/ | |  | |  __/_|     ___(/_ 0 0      |    |    
-:::intro:::  \____|_| |_|\__,_|___/\___| |_|  |_|\___(_)   *(     =(_T_)=    |    |    
+:::intro::: | |___| | | | (_| \__ \  __/ | |  | |  __/_|     ___(/_ 0 0      |LXD |    
+:::intro:::  \____|_| |_|\__,_|___/\___| |_|  |_|\___(_)   *(     =(_T_)=    | WIN|    
 :::intro:::                                                  \  )   \"\      \    /   
 :::intro:::            Linux, Python, vim & git!              |__>-\_>_>      \__/
 
@@ -49,16 +49,16 @@ cls
 :::two:::                                                 ()    \\
 :::two:::  This script will install one Linux under the     O    \\  .
 :::two:::  Windows Subsystem for Linux (WSL) and use it to    o  |\\/|
-:::two:::  host JupterLab accessible through your Windows        / " '\
-:::two:::  browser at http://localhost:8888 It will also         . .   .
-:::two:::  introduce you to Linux, Python, vim and git.         /    ) |
+:::two:::  host ANOTHER Linux under the LXD Container system     / " '\
+:::two:::  that will host JupyterLab, which will be accessible   . .   .
+:::two:::  from your Windows browser at http://localhost:8888   /    ) |
 :::two:::                                                      '  _.'  |
 :::two:::                           HYPERVISOR                 '-'/    \
 :::two:::   ____________________________ ______________________________
 :::two:::  |Windows 10 or 11           | |Windows Subsystem for Linux |
-:::two:::  |         ____ ____         | |Ubuntu 20.04                |
+:::two:::  |         ____ ____         | |Ubuntu 20.04 with LXD       |
 :::two:::  |      ,_/site\____\____.   | | __________________________ |
-:::two:::  |      |                |   | | |GNU screen terminal     | |
+:::two:::  |      |                |   | | |Ubuntu 20.04 (non-WSL)  | |
 :::two:::  |      | localhost:8888 -------->JupyterLab Server       | |
 :::two:::  |      |   ( browser )  |   | | |________________________| |
 :::two:::  |______|                |___| |____________________________|
@@ -93,6 +93,7 @@ cls
 for /f "delims=: tokens=1*" %%A in ('findstr /b ":::three:::" "%~f0"') do (echo.%%B)
 set /p warning= %
 
+timeout /t 2 /nobreak > NUL
 :::wee:::      'It  is  a long tail, certainly said Alice, looking down
 :::wee:::      with wonder at the Mouse's tail; 'but why do you call it
 :::wee:::      sad?'  And  she  kept  on  puzzling  about  it while the
@@ -156,6 +157,7 @@ set /p warning= %
 :::wee:::   Installing Ubuntu 20.04 under Windows Subsystem for Linux...
 :::wee:::
 for /f "delims=: tokens=1*" %%A in ('findstr /b ":::wee:::" "%~f0"') do (echo.%%B)
+timeout /t 2 /nobreak > NUL
 
 wsl --unregister Ubuntu-20.04
 wsl --set-default-version 2
@@ -168,10 +170,10 @@ set password="foo"
 ubuntu2004 install --root
 
 :: Once Ubuntu 20.04 is installed, this sets up the default user.
-wsl -d Ubuntu-20.04 -u root useradd -m "%wsluer%"
-wsl -d Ubuntu-20.04 -u root sh -c "echo "%wsluer%:%password%" | chpasswd" 
-wsl -d Ubuntu-20.04 -u root chsh -s /bin/bash "%wsluer%"
-wsl -d Ubuntu-20.04 -u root usermod -aG adm,cdrom,sudo,dip,plugdev,lxd "%wsluer%"
+wsl --distribution Ubuntu-20.04 -u root useradd -m "%wsluer%"
+wsl --distribution Ubuntu-20.04 -u root sh -c "echo "%wsluer%:%password%" | chpasswd" 
+wsl --distribution Ubuntu-20.04 -u root chsh -s /bin/bash "%wsluer%"
+wsl --distribution Ubuntu-20.04 -u root usermod -aG adm,cdrom,sudo,dip,plugdev,lxd "%wsluer%"
 ubuntu2004 config --default-user "%wsluer%"
 
 :: This creates "repos" folder in your Windows HOME for Windows/Linux file sharing.
@@ -186,66 +188,78 @@ if exist apt_installs.sh (copy apt_installs.sh %USERPROFILE%\repos\transfer) els
 if exist requirements.txt (copy requirements.txt %USERPROFILE%\repos\transfer) else (curl -L -o %USERPROFILE%\repos\transfer\requirements.txt "https://raw.githubusercontent.com/miklevin/lxdwin/main/requirements.txt")
 
 :: This makes file permissions under WSL keyed off of your Windows-side.
-wsl -d Ubuntu-20.04 -u root -e echo -e [boot]\nsystemd=true >> ./wsl.conf
-wsl -d Ubuntu-20.04 -u root -e mv wsl.conf /etc/
+:: wsl --distribution Ubuntu-20.04 -u root -e echo -e [automount]\noptions=\"metadata\" > ./wsl.conf
+wsl --distribution Ubuntu-20.04 -u root -e echo -e [boot]\nsystemd=true >> ./wsl.conf
+wsl --distribution Ubuntu-20.04 -u root -e mv wsl.conf /etc/
 wsl -t Ubuntu-20.04
 
 :: This creates the a repos, .ssh and .config folders on WSL by linking to your Windows-side.
-wsl -d Ubuntu-20.04 -e bash -lic "ln -s /mnt/c/Users/%USERNAME%/.ssh/ /home/ubuntu/.ssh" >NUL
-wsl -d Ubuntu-20.04 -e bash -lic "ln -s /mnt/c/Users/%USERNAME%/repos/ /home/ubuntu/repos" >NUL
-wsl -d Ubuntu-20.04 -e bash -lic "ln -s /mnt/c/Users/%USERNAME%/.config/ /home/ubuntu/.config" >NUL
+wsl --distribution Ubuntu-20.04 -e bash -lic "ln -s /mnt/c/Users/%USERNAME%/.ssh/ /home/ubuntu/.ssh" >NUL
+wsl --distribution Ubuntu-20.04 -e bash -lic "ln -s /mnt/c/Users/%USERNAME%/repos/ /home/ubuntu/repos" >NUL
+wsl --distribution Ubuntu-20.04 -e bash -lic "ln -s /mnt/c/Users/%USERNAME%/.config/ /home/ubuntu/.config" >NUL
 
 :: If you keep a .vimrc and .gitconfig Windows-side, this copies them over.
-wsl -d Ubuntu-20.04 -e bash -lic "cp /mnt/c/Users/%USERNAME%/.vimrc /home/ubuntu/" >NUL
-wsl -d Ubuntu-20.04 -e bash -lic "cp /mnt/c/Users/%USERNAME%/.gitconfig /home/ubuntu/" >NUL
+wsl --distribution Ubuntu-20.04 -e bash -lic "cp /mnt/c/Users/%USERNAME%/.vimrc /home/ubuntu/" >NUL
+wsl --distribution Ubuntu-20.04 -e bash -lic "cp /mnt/c/Users/%USERNAME%/.gitconfig /home/ubuntu/" >NUL
 
-:::apt:::  _   _           _       _   _                _     _                  
-:::apt::: | | | |_ __   __| | __ _| |_(_)_ __   __ _   | |   (_)_ __  _   ___  __
-:::apt::: | | | | '_ \ / _` |/ _` | __| | '_ \ / _` |  | |   | | '_ \| | | \ \/ /
-:::apt::: | |_| | |_) | (_| | (_| | |_| | | | | (_| |  | |___| | | | | |_| |>  < 
-:::apt:::  \___/| .__/ \__,_|\__,_|\__|_|_| |_|\__, |  |_____|_|_| |_|\__,_/_/\_\
+:::apt:::  _   _           _       _   _              __        ______  _       _     _                  
+:::apt::: | | | |_ __   __| | __ _| |_(_)_ __   __ _  \ \      / / ___|| |     | |   (_)_ __  _   ___  __
+:::apt::: | | | | '_ \ / _` |/ _` | __| | '_ \ / _` |  \ \ /\ / /\___ \| |     | |   | | '_ \| | | \ \/ /
+:::apt::: | |_| | |_) | (_| | (_| | |_| | | | | (_| |   \ V  V /  ___) | |___  | |___| | | | | |_| |>  < 
+:::apt:::  \___/| .__/ \__,_|\__,_|\__|_|_| |_|\__, |    \_/\_/  |____/|_____| |_____|_|_| |_|\__,_/_/\_\
 :::apt:::       |_|                            |___/                                                     
 for /f "delims=: tokens=1*" %%A in ('findstr /b ":::apt:::" "%~f0"') do (echo.%%B)
+timeout /t 2 /nobreak > NUL
 
 :: We update the software reposotory on the Ubuntu 20.04 Machine
-wsl -d Ubuntu-20.04 -u root -e sudo apt update
+wsl --distribution Ubuntu-20.04 -u root -e sudo apt update
 
 :: With Figlet installed, I no longer need to embed ASCII art headlines.
-wsl -d Ubuntu-20.04 -u root -e sudo apt install figlet
-wsl -d Ubuntu-20.04 -e bash -lic "figlet -t 'Upgrading Linux...'"
+wsl --distribution Ubuntu-20.04 -u root -e sudo apt install figlet
+wsl --distribution Ubuntu-20.04 -e bash -lic "figlet -t 'Upgrading WSL Linux...'"
+timeout /t 2 /nobreak > NUL
 
 :: And now the big upgrading of all the Ubuntu 20.04 software.
-wsl -d Ubuntu-20.04 -u root -e sudo apt upgrade -y
-wsl -d Ubuntu-20.04 -u root -e sudo apt install x11-apps -y
+wsl --distribution Ubuntu-20.04 -u root -e sudo apt upgrade -y
+wsl --distribution Ubuntu-20.04 -u root -e sudo apt install x11-apps -y
 
 :: Grab program that waits for Jupyter to start on Ubuntu 20.04 and put in sbin.
-wsl -d Ubuntu-20.04 -u root -e curl -L -o /usr/local/sbin/jupyterlogin "https://raw.githubusercontent.com/miklevin/lxdwin/main/jupyterlogin"
-wsl -d Ubuntu-20.04 -u root -e chown ubuntu:ubuntu /usr/local/sbin/jupyterlogin
-wsl -d Ubuntu-20.04 -u root -e chmod +x /usr/local/sbin/jupyterlogin
+wsl --distribution Ubuntu-20.04 -u root -e curl -L -o /usr/local/sbin/jupyterlogin "https://raw.githubusercontent.com/miklevin/lxdwin/main/jupyterlogin"
+wsl --distribution Ubuntu-20.04 -u root -e chown ubuntu:ubuntu /usr/local/sbin/jupyterlogin
+wsl --distribution Ubuntu-20.04 -u root -e chmod +x /usr/local/sbin/jupyterlogin
 
 :: You know what's nice? Not having to type a password every time you sudo a command!
-wsl -d Ubuntu-20.04 -u root /bin/bash -c "echo 'ubuntu	ALL=(ALL:ALL) NOPASSWD:ALL'> /etc/sudoers.d/ubuntu"
+wsl --distribution Ubuntu-20.04 -u root /bin/bash -c "echo 'ubuntu	ALL=(ALL:ALL) NOPASSWD:ALL'> /etc/sudoers.d/ubuntu"
+timeout /t 2 /nobreak > NUL
+
+:: Deep breath... Install the LXD Linux Container system on WSL accepting all defaults.
+wsl --distribution Ubuntu-20.04 -e bash -lic "figlet -t 'Installing LXD...'"
+wsl --distribution Ubuntu-20.04 -u ubuntu -e sudo snap refresh lxd --channel=latest
+wsl --distribution Ubuntu-20.04 -u ubuntu -e sudo lxd init --auto
 
 :: Grab and run second-half of install that runs under WSL and set up Linux graphics.
-wsl -d Ubuntu-20.04 -u ubuntu -e curl -L -o /home/ubuntu/nolxdinstall.sh "https://raw.githubusercontent.com/miklevin/lxdwin/main/nolxdinstall.sh"
-wsl -d Ubuntu-20.04 -e bash -lic "bash /home/ubuntu/nolxdinstall.sh"
-wsl -d Ubuntu-20.04 -e bash -lic "curl -L -o /home/ubuntu/.bash_profile 'https://raw.githubusercontent.com/miklevin/lxdwin/main/.bash_profile'"
-echo Returning from noinstall.sh, rebooting WSL for updated ACLs (access control list)
+wsl --distribution Ubuntu-20.04 -u ubuntu -e curl -L -o /home/ubuntu/install.sh "https://raw.githubusercontent.com/miklevin/lxdwin/main/install.sh"
+wsl --distribution Ubuntu-20.04 -e bash -lic "bash /home/ubuntu/install.sh"
+exit
+wsl --distribution Ubuntu-20.04 -e bash -lic "curl -L -o /home/ubuntu/.bash_profile 'https://raw.githubusercontent.com/miklevin/lxdwin/main/.bash_profile-wsl'"
+echo Returning from install.sh, rebooting WSL for updated ACLs (access control list)
 
 :: Grab post-reboot scripts. ACLs aren't sufficent for git cloning without a wsl --shutdown
+:: wsl --shutdown
+
 wsl -t Ubuntu-20.04
 
-wsl -d Ubuntu-20.04 -u root -e echo "Back from shutdown"
-wsl -d Ubuntu-20.04 -u root -e chmod 600 /home/ubuntu/.ssh/id_rsa_lxdwin
-wsl -d Ubuntu-20.04 -u root -e chmod 600 /home/ubuntu/.ssh/id_rsa_lxdwin.pub
-wsl -d Ubuntu-20.04 -u ubuntu -e curl -L -o /home/ubuntu/repos/transfer/git_installs.sh "https://raw.githubusercontent.com/miklevin/lxdwin/main/git_installs.sh"
-wsl -d Ubuntu-20.04 -u ubuntu -e sh /home/ubuntu/repos/transfer/git_installs.sh
+wsl --distribution Ubuntu-20.04 -u root -e echo "Back from shutdown"
+wsl --distribution Ubuntu-20.04 -u root -e chmod 600 /home/ubuntu/.ssh/id_rsa_lxdwin
+wsl --distribution Ubuntu-20.04 -u root -e chmod 600 /home/ubuntu/.ssh/id_rsa_lxdwin.pub
+wsl --distribution Ubuntu-20.04 -u ubuntu -e curl -L -o /home/ubuntu/repos/transfer/git_installs.sh "https://raw.githubusercontent.com/miklevin/lxdwin/main/git_installs.sh"
+wsl --distribution Ubuntu-20.04 -u ubuntu -e sh /home/ubuntu/repos/transfer/git_installs.sh
 
 :: Clean up after the install, deleting whatever is not intentionally left behind.
-wsl -d Ubuntu-20.04 -u root rm /home/ubuntu/nolxdinstall.sh
-wsl -d Ubuntu-20.04 -u root rm /home/ubuntu/repos/transfer/pub.txt
-wsl -d Ubuntu-20.04 -u root rm /home/ubuntu/repos/transfer/priv.txt
-wsl -d Ubuntu-20.04 -u root rm /home/ubuntu/repos/transfer/unrot.py
+wsl --distribution Ubuntu-20.04 -u root rm /home/ubuntu/install.sh
+wsl --distribution Ubuntu-20.04 -u root rm /home/ubuntu/repos/transfer/pub.txt
+wsl --distribution Ubuntu-20.04 -u root rm /home/ubuntu/repos/transfer/priv.txt
+wsl --distribution Ubuntu-20.04 -u root rm /home/ubuntu/repos/transfer/unrot.py
 
 set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
@@ -266,3 +280,7 @@ echo Double-click the icon "JupyterLab Login" created where you ran the script.
 echo.
 
 set /p warning=Press [Enter] to release this console window. %
+
+:: See the rest at https://raw.githubusercontent.com/miklevin/jupyme/main/install.sh
+:: but you don't have to do anything because it is downloaded and run from above.
+
