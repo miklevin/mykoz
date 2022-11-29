5daca8dee5e6d295493b2ea38ab4758e877e6dff
Mike Levin <miklevin@gmail.com>
2022-11-29 17:33:48 -0500

    Changed git repo to automatebrowser

--- git_installs.sh
+++ git_installs.sh
@@ -1,12 +1,9 @@
 figlet -t "Cloning git repos..."
 sleep 2
 cd /home/ubuntu/repos
-echo "Cloning ohawf..."
-[ ! -d "/home/ubuntu/repos/ohawf" ] && git clone git@github.com:miklevin/ohawf /home/ubuntu/repos/ohawf
+echo "Cloning automatebrowser..."
+[ ! -d "/home/ubuntu/repos/automatebrowser" ] && git clone git@github.com:miklevin/automatebrowser /home/ubuntu/repos/automatebrowser
 pip install -e /home/ubuntu/repos/ohawf
-echo "Cloning mlseo..."
-[ ! -d "/home/ubuntu/repos/mlseo" ] && git clone git@github.com:miklevin/mlseo /home/ubuntu/repos/mlseo
-pip install -e /home/ubuntu/repos/mlseo
 figlet -t "Done repo cloning."
 sleep 2
 
