1b867812e14653afda51d284d91aa06d9a434c51
Mike Levin <miklevin@gmail.com>
2022-11-30 18:54:28 -0500

    Changed repo name

--- git_installs.sh
+++ git_installs.sh
@@ -2,7 +2,7 @@ figlet -t "Cloning git repos..."
 sleep 2
 cd /home/ubuntu/repos
 echo "Cloning automatebrowser..."
-[ ! -d "/home/ubuntu/repos/automatebrowser" ] && git clone git@github.com:miklevin/automatebrowser /home/ubuntu/repos/automatebrowser
+[ ! -d "/home/ubuntu/repos/automatechrome" ] && git clone git@github.com:miklevin/automatechrome /home/ubuntu/repos/automatechrome
 pip install -e /home/ubuntu/repos/ohawf
 figlet -t "Done repo cloning."
 sleep 2
