9e5dcdae204969143f244c267a73c280fb2f1032
Mike Levin <miklevin@gmail.com>
2022-12-22 22:13:44 -0500

    Doing editable install of ohawf

--- git_installs.sh
+++ git_installs.sh
@@ -10,10 +10,10 @@ figlet -t "Comibine CSV Files..."
 figlet -t "Site Generator..."
 [ ! -d "/home/ubuntu/repos/skite" ] && git clone git@github.com:miklevin/skite /home/ubuntu/repos/skite
 
-# figlet -t "Google OAuth2..."
-# [ ! -d "/home/ubuntu/repos/ohawf" ] && git clone git@github.com:miklevin/ohawf /home/ubuntu/repos/ohawf
-# source /home/py311/bin/activate
-# /home/ubuntu/py311/bin/python3 -m pip install -e /home/ubuntu/repos/ohawf
+figlet -t "Google OAuth2..."
+[ ! -d "/home/ubuntu/repos/ohawf" ] && git clone git@github.com:miklevin/ohawf /home/ubuntu/repos/ohawf
+source /home/py311/bin/activate
+/home/ubuntu/py311/bin/python3 -m pip install -e[dev] /home/ubuntu/repos/ohawf
 
 figlet -t "Done repo cloning."
 
