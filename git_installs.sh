e8ddbe2bdd1c37b5fb2da0b313e6bcacc23c1713
Mike Levin <miklevin@gmail.com>
2022-12-30 14:21:55 -0500

    Added ohawf clone

--- git_installs.sh
+++ git_installs.sh
@@ -1,18 +1,18 @@
 figlet -t "Cloning git repos..."
 cd /home/ubuntu/repos
 
-figlet -t "Automate Chrome..."
+figlet -t "Pipulate FOSS SEO..."
 [ ! -d "/home/ubuntu/repos/pipulate" ] && git clone git@github.com:miklevin/automatechrome /home/ubuntu/repos/automatechrome
 
-figlet -t "Site Generator..."
+figlet -t "Skite SSGenerator..."
 [ ! -d "/home/ubuntu/repos/skite" ] && git clone git@github.com:miklevin/skite /home/ubuntu/repos/skite
 
-# figlet -t "Google OAuth2..."
-# [ ! -d "/home/ubuntu/repos/ohawf" ] && git clone git@github.com:miklevin/ohawf /home/ubuntu/repos/ohawf
-# source /home/py311/bin/activate
-# /home/ubuntu/py311/bin/python3 -m pip install -e /home/ubuntu/repos/ohawf
-
-playwright install
+figlet -t "Google OAuth2..."
+[ ! -d "/home/ubuntu/repos/ohawf" ] && git clone git@github.com:miklevin/ohawf /home/ubuntu/repos/ohawf
 
 figlet -t "Done repo cloning."
+
+figlet -t "Playwright Install..."
+playwright install
+
 
