08f295cdcdfab011a831d701002ecaac01562309
Mike Levin <miklevin@gmail.com>
2022-12-30 14:42:40 -0500

    Fixed git_installs

--- git_installs.sh
+++ git_installs.sh
@@ -2,10 +2,10 @@ figlet -t "Cloning git repos..."
 cd /home/ubuntu/repos
 
 figlet -t "Pipulate FOSS SEO..."
-[ ! -d "/home/ubuntu/repos/pipulate" ] && git clone git@github.com:miklevin/automatechrome /home/ubuntu/repos/pipulate
+[ ! -d "/home/ubuntu/repos/pipulate" ] && git clone git@github.com:miklevin/piplate /home/ubuntu/repos/pipulate
 
 figlet -t "Practice folder..."
-[ ! -d "/home/ubuntu/repos/practice" ] && git clone git@github.com:miklevin/ohawf /home/ubuntu/repos/practice
+[ ! -d "/home/ubuntu/repos/practice" ] && git clone git@github.com:miklevin/practice /home/ubuntu/repos/practice
 
 figlet -t "Skite SSGenerator..."
 [ ! -d "/home/ubuntu/repos/skite" ] && git clone git@github.com:miklevin/skite /home/ubuntu/repos/skite
