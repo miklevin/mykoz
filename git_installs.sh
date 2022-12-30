2041f0b9040af4a46eb04fb0387efc283bfdca37
Mike Levin <miklevin@gmail.com>
2022-12-30 14:17:34 -0500

    Added pipulate to git cloning

--- git_installs.sh
+++ git_installs.sh
@@ -2,10 +2,7 @@ figlet -t "Cloning git repos..."
 cd /home/ubuntu/repos
 
 figlet -t "Automate Chrome..."
-[ ! -d "/home/ubuntu/repos/automatechrome" ] && git clone git@github.com:miklevin/automatechrome /home/ubuntu/repos/automatechrome
-
-figlet -t "Comibine CSV Files..."
-[ ! -d "/home/ubuntu/repos/combinecsvfiles" ] && git clone git@github.com:miklevin/combinecsvfiles /home/ubuntu/repos/combinecsvfiles
+[ ! -d "/home/ubuntu/repos/pipulate" ] && git clone git@github.com:miklevin/automatechrome /home/ubuntu/repos/automatechrome
 
 figlet -t "Site Generator..."
 [ ! -d "/home/ubuntu/repos/skite" ] && git clone git@github.com:miklevin/skite /home/ubuntu/repos/skite
@@ -14,6 +11,8 @@ figlet -t "Site Generator..."
 # [ ! -d "/home/ubuntu/repos/ohawf" ] && git clone git@github.com:miklevin/ohawf /home/ubuntu/repos/ohawf
 # source /home/py311/bin/activate
 # /home/ubuntu/py311/bin/python3 -m pip install -e /home/ubuntu/repos/ohawf
+
+playwright install
 
 figlet -t "Done repo cloning."
 
