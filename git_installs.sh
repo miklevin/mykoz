5cd9914cc8cf443d04b2ecee07fb2e14084c9241
Mike Levin <miklevin@gmail.com>
2022-12-30 14:26:44 -0500

    Added practice folder

--- git_installs.sh
+++ git_installs.sh
@@ -2,7 +2,10 @@ figlet -t "Cloning git repos..."
 cd /home/ubuntu/repos
 
 figlet -t "Pipulate FOSS SEO..."
-[ ! -d "/home/ubuntu/repos/pipulate" ] && git clone git@github.com:miklevin/automatechrome /home/ubuntu/repos/automatechrome
+[ ! -d "/home/ubuntu/repos/pipulate" ] && git clone git@github.com:miklevin/automatechrome /home/ubuntu/repos/pipulate
+
+figlet -t "Practice folder..."
+[ ! -d "/home/ubuntu/repos/practice" ] && git clone git@github.com:miklevin/ohawf /home/ubuntu/repos/practice
 
 figlet -t "Skite SSGenerator..."
 [ ! -d "/home/ubuntu/repos/skite" ] && git clone git@github.com:miklevin/skite /home/ubuntu/repos/skite
