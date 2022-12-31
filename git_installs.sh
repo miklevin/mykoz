6f49c1b066a1148594e532e6b804a82947b8603b
Mike Levin <miklevin@gmail.com>
2022-12-30 19:45:18 -0500

    Removed ohawf

--- git_installs.sh
+++ git_installs.sh
@@ -10,9 +10,6 @@ figlet -t "Practice folder..."
 figlet -t "Skite SSGenerator..."
 [ ! -d "/home/ubuntu/repos/skite" ] && git clone git@github.com:miklevin/skite /home/ubuntu/repos/skite
 
-figlet -t "Google OAuth2..."
-[ ! -d "/home/ubuntu/repos/ohawf" ] && git clone git@github.com:miklevin/ohawf /home/ubuntu/repos/ohawf
-
 figlet -t "Done repo cloning."
 
 figlet -t "Playwright Install..."
