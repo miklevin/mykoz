99d77f0752de81a5b554ba17798bf20dcc0493e4
Mike Levin <miklevin@gmail.com>
2023-03-28 17:53:39 -0400

    Stripped down

--- apt_installs.sh
+++ apt_installs.sh
@@ -1,13 +1,13 @@
 # Standard bash file for doing late-stage apt installs
 
-figlet -t Installing Google Chrome
-sudo apt-get install libxss1 libappindicator1 libindicator7 -y
-cd /tmp
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome*.deb -y
-sudo apt install x11-apps -y
-sudo apt install libdbus-glib-1-2 -y 
-
-# Install Microsoft Playright browsers, edited out in favor of Chrome
-playwright install
+# figlet -t Installing Google Chrome
+# sudo apt-get install libxss1 libappindicator1 libindicator7 -y
+# cd /tmp
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo apt install ./google-chrome*.deb -y
+# sudo apt install x11-apps -y
+# sudo apt install libdbus-glib-1-2 -y 
+# 
+# # Install Microsoft Playright browsers, edited out in favor of Chrome
+# playwright install
 
--- git_installs.sh
+++ git_installs.sh
@@ -1,16 +1,16 @@
-figlet -t "Cloning git repos..."
-cd /home/ubuntu/repos
-
-figlet -t "Pipulate..."
-[ ! -d "/home/ubuntu/repos/pipulate" ] && git clone git@github.com:miklevin/pipulate /home/ubuntu/repos/pipulate
-
-figlet -t "Practice..."
-[ ! -d "/home/ubuntu/repos/practice" ] && git clone git@github.com:miklevin/practice /home/ubuntu/repos/practice
-
-figlet -t "Skite..."
-[ ! -d "/home/ubuntu/repos/skite" ] && git clone git@github.com:miklevin/skite /home/ubuntu/repos/skite
-
-figlet -t "Playwright..."
-/home/ubuntu/py311/bin/playwright install
+# figlet -t "Cloning git repos..."
+# cd /home/ubuntu/repos
+# 
+# figlet -t "Pipulate..."
+# [ ! -d "/home/ubuntu/repos/pipulate" ] && git clone git@github.com:miklevin/pipulate /home/ubuntu/repos/pipulate
+# 
+# figlet -t "Practice..."
+# [ ! -d "/home/ubuntu/repos/practice" ] && git clone git@github.com:miklevin/practice /home/ubuntu/repos/practice
+# 
+# figlet -t "Skite..."
+# [ ! -d "/home/ubuntu/repos/skite" ] && git clone git@github.com:miklevin/skite /home/ubuntu/repos/skite
+# 
+# figlet -t "Playwright..."
+# /home/ubuntu/py311/bin/playwright install
 
 
