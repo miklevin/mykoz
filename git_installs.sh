5d88703f96ba1a086d008ac50fabcff21ed2c4d8
Mike Levin <miklevin@gmail.com>
2023-04-04 17:38:46 -0400

    Edited in moz repo

--- git_installs.sh
+++ git_installs.sh
@@ -7,6 +7,9 @@
 # figlet -t "Practice..."
 # [ ! -d "/home/ubuntu/repos/practice" ] && git clone git@github.com:miklevin/practice /home/ubuntu/repos/practice
 # 
+figlet -t "MOZ Repo..."
+[ ! -d "/home/ubuntu/repos/moz" ] && git clone git@github.com:miklevin/moz /home/ubuntu/repos/moz
+# 
 # figlet -t "Skite..."
 # [ ! -d "/home/ubuntu/repos/skite" ] && git clone git@github.com:miklevin/skite /home/ubuntu/repos/skite
 # 
