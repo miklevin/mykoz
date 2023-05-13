ddc86e3f898e535fcdfefacfd983829672378dec
Mike Levin <miklevin@gmail.com>
2023-05-12 20:52:31 -0400

    Script grabber update

--- get_installer.bat
+++ get_installer.bat
@@ -1,12 +1,6 @@
-:: Write a bash script that will fetch a bash script from the internet and save it side-by-side with this file
-
 @echo off
-setlocal
-:: Set the URL of the bash script to fetch
-set URL=https://raw.githubusercontent.com/miklevin/drinkme/main/install.bat
-:: Set the name of the bash script to fetch
-:: This is the name of the file that will be saved side-by-side with this file
-set FILENAME=install.bat
-:: Fetch the script and save it
-curl -s -o %FILENAME% %URL%
-
+setlocal enableDelayedExpansion
+set /a "COUNTER+=1"
+set "URL=https://raw.githubusercontent.com/miklevin/drinkme/main/install.bat?cache=!COUNTER!"
+set "FILENAME=install.bat"
+curl -s -o "%FILENAME%" "%URL%" -H "Cache-Control: no-cache, no-store, must-revalidate" -H "Pragma: no-cache" -H "Expires: 0" --proxy-header "Cache-Control: no-cache" --proxy-header "Cache-Control: no-store"
