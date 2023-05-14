dda497977ce40067a068b23921cce3b6b323a988
Mike Levin <miklevin@gmail.com>
2023-05-14 18:59:03 -0400

    A very worthy 0.7.0

--- apt_installs.sh
+++ apt_installs.sh
@@ -1,12 +1,12 @@
 # Standard bash file for doing late-stage apt installs
 
 figlet -t NeoVim
-echo  Type with a sign in ages hence, NeoVim made all the difference.
-sudo apt install neovim -y
+echo  You will type with a sign in ages hence: NeoVim made all the difference.
+sudo apt install neovim -y > /dev/null 2>&1
 git clone https://github.com/github/copilot.vim.git /home/ubuntu/.config/nvim/pack/github/start/copilot.vim > /dev/null 2>&1
 
-figlet -t Google Chrome
-echo  What is the use of a Distro without Graphics or a Browser in it?
+figlet -t Chrome
+echo  For what good is a Linux Distro without Graphics or a Web Browser in it?
 sudo apt-get install libxss1 libappindicator1 libindicator7 -y > /dev/null 2>&1
 cd /tmp > /dev/null 2>&1
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
