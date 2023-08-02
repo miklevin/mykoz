eaef26495f2dae79aed8f975623c1dcdf8df2274
Mike Levin <miklevin@gmail.com>
2023-08-01 23:11:46 -0400

    Fixing drinkme

--- drinkme.bat
+++ drinkme.bat
@@ -2,7 +2,13 @@
 setlocal enableDelayedExpansion
 set "TIME=%TIME::=%"
 set "TIME=%TIME:.=%"
-set /a "COUNTER=!TIME!"
-set "URL=https://raw.githubusercontent.com/miklevin/drinkme/main/install.bat?cache=!COUNTER!"
+set /a "T=%TIME%"
+set "URL=https://raw.githubusercontent.com/miklevin/drinkme/main/install.bat?cache=%T%"
 set "FILENAME=install.bat"
-curl -s -o "%FILENAME%" "%URL%" -H "Cache-Control: no-cache, no-store, must-revalidate" -H "Pragma: no-cache" -H "Expires: 0" --proxy-header "Cache-Control: no-cache" --proxy-header "Cache-Control: no-store"
+curl -s -o "%FILENAME%" "%URL%" ^
+    -H "Cache-Control: no-cache, no-store, must-revalidate" ^
+    -H "Pragma: no-cache" ^
+    -H "Expires: 0" ^
+    --proxy-header "Cache-Control: no-cache" ^
+    --proxy-header "Cache-Control: no-store"
+
