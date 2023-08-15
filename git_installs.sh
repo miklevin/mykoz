2666f69a43cb2a6d6a7a83fa9140f923a9d2554f
Mike Levin <miklevin@gmail.com>
2023-08-15 13:09:08 -0400

    Git install changes for lowercase

--- git_installs.sh
+++ git_installs.sh
@@ -1,6 +1,6 @@
-[ ! -d "/home/ubuntu/repos/Journal" ] && GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git clone git@github.com:miklevin/Journal /home/ubuntu/repos/Journal
-[ ! -d "/home/ubuntu/repos/Intro" ] && GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git clone git@github.com:miklevin/Intro /home/ubuntu/repos/Intro
-[ ! -d "/home/ubuntu/repos/Service" ] && GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git clone git@github.com:miklevin/Service /home/ubuntu/repos/Service
+[ ! -d "/home/ubuntu/repos/journal" ] && GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git clone git@github.com:miklevin/journal /home/ubuntu/repos/journal
+[ ! -d "/home/ubuntu/repos/intro" ] && GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git clone git@github.com:miklevin/intro /home/ubuntu/repos/intro
+[ ! -d "/home/ubuntu/repos/yourservice" ] && GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git clone git@github.com:miklevin/yourservice /home/ubuntu/repos/yourservice
 
 # figlet -t "Playwright..."
 # /home/ubuntu/py311/bin/playwright install
