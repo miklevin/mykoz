09d113493974f1bbc93b052e4f06f606e3ee05c2
Mike Levin <miklevin@gmail.com>
2023-08-16 17:16:23 -0400

    Testing with Python 3.12

--- install.bat
+++ install.bat
@@ -36,7 +36,7 @@ cls
 REM SET UP ENVIRONMENT VARIABLES
 set giturl=https://raw.githubusercontent.com/miklevin/mykoz/main/
 set mykoz=0.9.87
-set python=3.11
+set python=3.12
 set "wsl_status="
 
 REM CHECK IF WSL IS INSTALLED
